596bcae0b1e037893aa1b1ae0d4c2400c63811f3
Mark Johnson <mark@jhns.in>
2022-05-05 16:06:31 -0500

    finished react router segment

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,26 @@
 import React from "react";
-import HomePage from './HomePage';
-import AboutPage from './AboutPage';
-import CoursesPage from './CoursesPage';
-import Header from './common/Header';
-import { Route, Switch } from 'react-router-dom';
+import HomePage from "./HomePage";
+import AboutPage from "./AboutPage";
+import CoursesPage from "./CoursesPage";
+import Header from "./common/Header";
+import { Route, Switch, Redirect } from "react-router-dom";
 import NotFoundPage from "./NotFoundPage";
+import ManageCoursePage from "./ManageCoursePage";
 
 function App() {
-    return (
-        <div className="container-fluid">
-            <Header />
-            <Switch>
-                <Route path="/" exact component={HomePage} />
-                <Route path="/courses" component={CoursesPage} />
-                <Route path="/about" component={AboutPage} />
-                <Route component={NotFoundPage} />
-            </Switch>
-        </div>
-    );
+  return (
+    <div className="container-fluid">
+      <Header />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route path="/course/:slug" component={ManageCoursePage} />
+        <Route path="/about" component={AboutPage} />
+        <Redirect from="/about-page" to="about" />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
-import  CourseList  from "./CourseList";
+import CourseList from "./CourseList";
 
 function CoursesPage() {
-    const [ courses, setCourses] = useState([]);
+  const [courses, setCourses] = useState([]);
 
-    useEffect(() => {
-        getCourses().then(_courses  => setCourses(_courses));
-    }, []);
+  useEffect(() => {
+    getCourses().then((_courses) => setCourses(_courses));
+  }, []);
 
-    return <>
-        <h2>Courses</h2>
-        <CourseList courses={courses} />
+  return (
+    <>
+      <h2>Courses</h2>
+      <CourseList courses={courses} />
     </>
+  );
 }
 
 export default CoursesPage;
